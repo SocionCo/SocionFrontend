70a439a67909e10ede936469b3671e87e4bb413c
dbiundo09 <biundo@bc.edu>
2023-08-11 23:32:33 +0200

    Url

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
-module: {
+const path = require('path');
+
+module.exports = {
+  // Entry file
+  entry: './src/index.js',
+
+  // Output configuration
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  // Module rules
+  module: {
     rules: [
+      // JavaScript and JSX files
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -10,6 +24,7 @@ module: {
           },
         },
       },
+      // Images
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
@@ -21,6 +36,19 @@ module: {
           },
         ],
       },
-    ]
-  }
-  
+      // Add other rules as needed (e.g., CSS, SCSS)
+    ],
+  },
+
+  // Resolve extensions
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  // Development server configuration
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
