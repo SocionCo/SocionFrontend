e173a4118ad68842cf02762af03e62ac385d47f5
dbiundo09 <biundo@bc.edu>
2024-01-15 20:42:39 -0500

    Terms Added

--- src/services/apiString.js
+++ src/services/apiString.js
@@ -8,4 +8,4 @@ export default apiString;
 
 export const PrivacyPolicyLink = "https://socion-draft-bucket.s3.us-east-2.amazonaws.com/Socion+-+Privacy+Policy+(Ballard+Version).DOCX+(1).pdf";
 
-export const TermsLink = "https://socion-draft-bucket.s3.us-east-2.amazonaws.com/Socion+-+Terms+(Ballard+Version).DOCX+(1).pdf";
+export const TermsLink = "https://socion-draft-bucket.s3.us-east-2.amazonaws.com/Socion+-+Terms+(Ballard+Version).DOCX+(2).pdf";
